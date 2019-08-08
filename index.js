0707816347258c2eeb68790d7082f88d0ea1e946
teej387 <teej387@yahoo.com>
2019-08-08 16:08:47 -0500

    Created put handler for /api/users/:id

--- index.js
+++ index.js
@@ -73,7 +73,32 @@ server.delete('/api/users/:id', (req, res) => {
     })
 })
 
+server.put('/api/users/:id', (req, res) => {
+  if (!req.body.name || !req.body.bio) {
+    res.status(400).json({
+      errorMessage: 'Please provide name and bio for the user.'
+    })
+  }
+  else {
+    db.update(req.params.id, req.body)
+      .then(user => {
+        if (user) {
+          res.status(200).json(user)
+        }
+        else {
+          res.status(404).json({
+            message: 'The user with the specified ID does not exist.'
+          })
+        }
+      })
+      .catch(() => {
+        res.status(500).json({
+          errorMessage: 'The user information could not be modified.'
+        })
+      })
 
+  }
+})
 
 server.listen(8000, () => {
   console.log('Server is running on port 8000...')
