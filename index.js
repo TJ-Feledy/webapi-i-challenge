37861297717efbed428a260a20f74f589f3485b4
teej387 <teej387@yahoo.com>
2019-08-07 21:58:02 -0500

    Created post handler for api/users

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const express = require('express')
+const db = require('./data/db.js')
+const server = express()
 
+server.use(express.json())
+
+server.post('/api/users', (req, res) => {
+  if (!req.body.name || !req.name.bio) {
+    res.status(400).json({ errorMessage: 'Please provide name and bio for the user.' })
+  }
+  else {
+    db.insert(req.body)
+      .then(user => {
+        res.status(201).json(user)
+      })
+      .catch(() => {
+        re.status(500).json({
+          errorMessage: 'There was an error while saving the user to the database.'
+        })
+      })
+  }
+})
+
+server.listen(8000, () => {
+  console.log('Server is running on port 8000...')
+})
